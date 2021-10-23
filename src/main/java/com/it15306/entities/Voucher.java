82313ae39d68557c188e01bfba8cfe11e477ecbc
khanhngoc2009 <khanhpvph10443@fpt.ed.vn>
2021-10-23 09:43:59 +0700

    update voucher

--- src/main/java/com/it15306/entities/Voucher.java
+++ src/main/java/com/it15306/entities/Voucher.java
@@ -58,13 +58,94 @@ public class Voucher {
 	
 	@OneToMany(mappedBy = "voucher")
 	private List<Order> cart = new ArrayList<>();
+
+	public Integer getVoucher_id() {
+		return voucher_id;
+	}
+
+	public void setVoucher_id(Integer voucher_id) {
+		this.voucher_id = voucher_id;
+	}
+
+	public Date getCreate_time() {
+		return create_time;
+	}
+
+	public void setCreate_time(Date create_time) {
+		this.create_time = create_time;
+	}
+
+	public Date getStart_time() {
+		return start_time;
+	}
+
+	public void setStart_time(Date start_time) {
+		this.start_time = start_time;
+	}
+
+	public Date getEnd_time() {
+		return end_time;
+	}
+
+	public void setEnd_time(Date end_time) {
+		this.end_time = end_time;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public Integer getDiscount() {
+		return discount;
+	}
+
+	public void setDiscount(Integer discount) {
+		this.discount = discount;
+	}
+
+	public Integer getType_discount() {
+		return type_discount;
+	}
+
+	public void setType_discount(Integer type_discount) {
+		this.type_discount = type_discount;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public List<Order> getCart() {
+		return cart;
+	}
+
+	public void setCart(List<Order> cart) {
+		this.cart = cart;
+	}
 	
-//	public List<Order> getOrders() {
-//		return orders;
-//	}
-//
-//	public void setOrders(List<Order> orders) {
-//		this.orders = orders;
-//	}
 	
 }
