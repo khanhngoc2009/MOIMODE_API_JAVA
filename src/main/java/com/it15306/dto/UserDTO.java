ba23c158ad2745d0689fcc3fedf693079ea00a00
khanhngoc2009 <khanhpvph10443@fpt.ed.vn>
2021-11-02 23:48:43 +0700

    update user DTO

--- src/main/java/com/it15306/dto/UserDTO.java
+++ src/main/java/com/it15306/dto/UserDTO.java
@@ -116,19 +116,24 @@ private Integer id;
 	public void setId(Integer id) {
 		this.id = id;
 	}
-	public UserDTO(Integer id, String email, String username, Integer activated, String photo) {
-		super();
-		this.id = id;
-		this.email = email;
-		this.username = username;
-		this.activated = activated;
-		this.photo = photo;
-	}
+//	public UserDTO(Integer id, String email, String username, Integer activated, String photo) {
+//		super();
+//		this.id = id;
+//		this.email = email;
+//		this.username = username;
+//		this.activated = activated;
+//		this.photo = photo;
+//	}
+//	public UserDTO() {
+//		super();
+//	}
 	
-	public UserDTO() {
-		
-	}
+//	public UserDTO() {
+////		super();
+//	}
+	
 
+	
 	
 	
 }
