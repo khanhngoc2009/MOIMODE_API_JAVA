da73679d9836b72d7b1abb68dcf7811482ae7016
Nguyễn Ngọc Đang <68001641+nguyenngocdang00@users.noreply.github.com>
2021-11-29 22:10:55 +0700

    create api thông ke doanh thu, thông ke đơn hàng

---
+++ src/main/java/com/it15306/dto/dashboard/ThongKeBody.java
@@ -0,0 +1,72 @@
+package com.it15306.dto.dashboard;
+
+public class ThongKeBody {
+	
+	public String startDate;
+	public String endDate;
+	public Integer status;
+	public Integer page;
+	public Integer take;
+	/**
+	 * @return the startDate
+	 */
+	public String getStartDate() {
+		return startDate;
+	}
+	/**
+	 * @param startDate the startDate to set
+	 */
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+	/**
+	 * @return the endDate
+	 */
+	public String getEndDate() {
+		return endDate;
+	}
+	/**
+	 * @param endDate the endDate to set
+	 */
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+	/**
+	 * @return the status
+	 */
+	public Integer getStatus() {
+		return status;
+	}
+	/**
+	 * @param status the status to set
+	 */
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+	/**
+	 * @return the page
+	 */
+	public Integer getPage() {
+		return page;
+	}
+	/**
+	 * @param page the page to set
+	 */
+	public void setPage(Integer page) {
+		this.page = page;
+	}
+	/**
+	 * @return the take
+	 */
+	public Integer getTake() {
+		return take;
+	}
+	/**
+	 * @param take the take to set
+	 */
+	public void setTake(Integer take) {
+		this.take = take;
+	}
+	
+	
+}
