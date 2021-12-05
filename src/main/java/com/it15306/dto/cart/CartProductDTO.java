7bec2d1ad45ef6fe9be8b3c65e3b4a1a7fc61a2e
khanhngoc2009 <khanhpvph10443@fpt.ed.vn>
2021-12-05 20:01:28 +0700

    update cart

--- src/main/java/com/it15306/dto/cart/CartProductDTO.java
+++ src/main/java/com/it15306/dto/cart/CartProductDTO.java
@@ -9,6 +9,22 @@ public class CartProductDTO {
 	private Integer quantity;
 	private ProductSkuDto ProductSkuDTOs;
 	private CartDTO CartDTO;
+	private String product_name;
+	private String option_value;
+	
+	
+	public String getProduct_name() {
+		return product_name;
+	}
+	public void setProduct_name(String product_name) {
+		this.product_name = product_name;
+	}
+	public String getOption_value() {
+		return option_value;
+	}
+	public void setOption_value(String option_value) {
+		this.option_value = option_value;
+	}
 	/**
 	 * @return the id
 	 */
