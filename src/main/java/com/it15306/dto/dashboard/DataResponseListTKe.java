6277cd07dabecd634923a76d19668b94f8de0ef7
Nguyễn Ngọc Đang <68001641+nguyenngocdang00@users.noreply.github.com>
2021-12-07 20:40:33 +0700

    update api thong ke

---
+++ src/main/java/com/it15306/dto/dashboard/DataResponseListTKe.java
@@ -0,0 +1,75 @@
+package com.it15306.dto.dashboard;
+
+import java.util.List;
+
+public class DataResponseListTKe<T>{
+	private Integer code;
+	private String message;
+	private List<T> listData;
+	private Float tong;
+	private Integer countTotalElement;
+	
+	
+	/**
+	 * @return the countTotalElement
+	 */
+	public Integer getCountTotalElement() {
+		return countTotalElement;
+	}
+	/**
+	 * @param countTotalElement the countTotalElement to set
+	 */
+	public void setCountTotalElement(Integer countTotalElement) {
+		this.countTotalElement = countTotalElement;
+	}
+	/**
+	 * @return the code
+	 */
+	public Integer getCode() {
+		return code;
+	}
+	/**
+	 * @param code the code to set
+	 */
+	public void setCode(Integer code) {
+		this.code = code;
+	}
+	/**
+	 * @return the message
+	 */
+	public String getMessage() {
+		return message;
+	}
+	/**
+	 * @param message the message to set
+	 */
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	/**
+	 * @return the listData
+	 */
+	public List<T> getListData() {
+		return listData;
+	}
+	/**
+	 * @param listData the listData to set
+	 */
+	public void setListData(List<T> listData) {
+		this.listData = listData;
+	}
+	/**
+	 * @return the tong
+	 */
+	public Float getTong() {
+		return tong;
+	}
+	/**
+	 * @param tong the tong to set
+	 */
+	public void setTong(Float tong) {
+		this.tong = tong;
+	}
+	
+	
+}
